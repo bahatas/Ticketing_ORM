c8c523929fb56152fe91437a51ec6751adf45902
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-06-08 23:35:06 +0100

    Spring Orm Ticketing

---
+++ src/main/java/com/ticketing/entity/Project.java
@@ -0,0 +1,34 @@
+package com.ticketing.entity;
+
+
+import com.ticketing.utils.Status;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name ="projects")
+@NoArgsConstructor
+@Getter
+@Setter
+@Where(clause = "is_deleted=false")
+
+public class Project extends BaseEntity{
+
+    private String projectCode;
+    private String projectName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manager_id")
+    private User assignedManager;
+
+    private LocalDate startdate;
+    private LocalDate endDate;
+    private Status projectStatus;
+    private String projectDetail;
+
+}
